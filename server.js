dabb8814a18695b1b576bfcd46fe61756cabd0ec
Esteban Pedraza <estebanp@bextsa.com>
2020-11-26 12:00:04 -0500

    Primera version 1.0

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+//helpers
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'Esteban PEDRAZA',
+        puerto: port
+    });
+
+});
+
+app.get('/about', function(req, res) {
+
+    res.render('about', {
+
+    });
+
+});
+
+app.get('/data', function(req, res) {
+
+    res.send('Hola data');
+
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
